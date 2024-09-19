b46bcfbf7a29c79f2d34a1169b4061664c9c5202
glebbykadorov <43245014+glebbykadorov@users.noreply.github.com>
2024-09-19 11:39:51 +0300

    Update task.py

--- task1/task.py
+++ task1/task.py
@@ -5,7 +5,6 @@ tree_json = json.load(json_tree_file)
     
 def dict_to_tree_req(tree_json, tree ,prev ,current): 
     for node in tree_json:
-        print(node)
         tree[0].append(node)
         tree[1].append(prev)
         current[0] += 1
