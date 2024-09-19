3fb6520eb9dc5f0a745bca1cce85ebf9f2e58780
glebbykadorov <43245014+glebbykadorov@users.noreply.github.com>
2024-09-19 05:50:55 +0300

    Add files via upload

---
+++ task1/task.py
@@ -0,0 +1,31 @@
+import json
+
+json_tree_file = open('tree.json')
+tree_json = json.load(json_tree_file)
+    
+def dict_to_tree_req(tree_json, tree ,prev ,current): 
+    for node in tree_json:
+        print(node)
+        tree[0].append(node)
+        tree[1].append(prev)
+        current[0] += 1
+        if tree_json[node] != - 1:
+            dict_to_tree_req(tree_json[node],tree,current[0] - 1,current)
+
+class tree:
+    def __init__(self,labels,parents):
+        self.labels = labels
+        self.parents = parents
+    def __str__(self):
+        return str(self.labels) + '\n' + str(self.parents)
+
+def dict_to_tree(tree_json):
+    data = ([],[])
+    prev = -1
+    current = [0]
+    dict_to_tree_req(tree_json, data ,prev, current)
+    a = tree(data[0],data[1])
+    return a
+
+print(dict_to_tree(tree_json))
+
